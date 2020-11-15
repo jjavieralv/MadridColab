aa4476ccc1a398b3dda4b33dfbd00b0fe02acdf4
Juan Sebastian Ochoa ZAmbrano <juansebastian.ochoa@satec.es>
2020-11-15 17:17:57 +0100

    Update FusionController.java
    Added new routes. Use some toy data to display in the view

--- madridColab/view/src/main/java/org/xcolab/view/pages/fusion/view/FusionController.java
+++ madridColab/view/src/main/java/org/xcolab/view/pages/fusion/view/FusionController.java
@@ -6,36 +6,81 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.xcolab.client.fusion.FusionClient;
+import org.xcolab.client.fusion.beans.FusionBean;
+import org.xcolab.client.fusion.utils.FusionStatus;
+import org.xcolab.client.members.exceptions.MemberNotFoundException;
 import org.xcolab.client.members.pojo.Member;
+import org.xcolab.view.pages.fusion.beans.FusionRequestBean;
 
-import javax.jws.WebParam.Mode;
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @Controller
 public class FusionController {
 
+    FusionClient fusionClient;
+    private boolean sent=false;
+
     @GetMapping("/fusionRequests")
-    public String getMyFusions(HttpServletResponse response, Model model,
+    public String getReceivedFusions(HttpServletResponse response, Model model,
             Integer pageNumber, Member loggedInMember){
 
+        fusionClient= new FusionClient();
+        FusionBean fusionBean;
+        try {
+            fusionBean=fusionClient.createFusionBean((long)1, (long)10148,(long)10146, (long)8, (long)9,
+                    FusionStatus.PENDING, null, null, "", "");
+        }catch (MemberNotFoundException e){
+            fusionBean=null;
+        }
+        ArrayList<FusionBean> fusions= new ArrayList<>();
+        fusions.add(fusionBean);
+        fusionBean.setStatus(FusionStatus.ACCEPTED);
+        fusions.add(fusionBean);
+        fusionBean.setStatus(FusionStatus.REJECTED);
+        fusions.add(fusionBean);
+        sent=false;
+        model.addAttribute("fusionRequests", fusions);
+        model.addAttribute("user", loggedInMember);
+        model.addAttribute("sent", sent);
+        return "/fusion/fusionRequests";
+    }
+
+    @GetMapping("/fusionRequests/sent")
+    public String getSentFusions(HttpServletResponse response, Model model,
+            Integer pageNumber, Member loggedInMember){
+        sent=true;
+        model.addAttribute("user", loggedInMember);
+        model.addAttribute("sent", sent);
         return "/fusion/fusionRequests";
     }
 
     @GetMapping("/fusionRequest/{proposalId}")
     public String newFusionRequest(HttpServletResponse response, Model model,
-            @PathVariable Integer proposalId){
+            @PathVariable Integer proposalId, Member loggedInMember){
         System.out.println(proposalId);
+        fusionClient= new FusionClient();
+        FusionBean fusionBean;
+        try {
+            fusionBean=fusionClient.createFusionBean((long)1, (long)10148,(long)10146, (long)8, (long)9,
+                    FusionStatus.PENDING, null, null, "", "");
+        }catch (MemberNotFoundException e){
+            fusionBean=null;
+        }
+        fusionBean.setRequestText("<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>");
 
-        return "/fusion/newFusionRequest";
+        model.addAttribute("user", loggedInMember);
+        model.addAttribute("fusionBean", fusionBean);
+        return "/fusion/fusionRequest";
     }
 
-    @PostMapping("/fusionRequest")
-    public String fusionRequest(){
+    @PostMapping("/fusionRequest/api/new")
+    public String fusionRequest(@Valid FusionRequestBean fusionRequestBean,
+            HttpServletResponse response, Model model, Member loggedInMember){
+        System.out.println(fusionRequestBean);
         return null;
     }
-
-
-
-
-
 }
