2c423e378a7ab5b77350f95cdd9e71a5add11081
Juan Sebastian Ochoa ZAmbrano <juansebastian.ochoa@satec.es>
2020-11-25 19:11:51 +0100

    Update FusionStatus.java

--- madridColab/microservices/clients/contestproposal-client/src/main/java/org/xcolab/client/fusion/utils/FusionStatus.java
+++ madridColab/microservices/clients/contestproposal-client/src/main/java/org/xcolab/client/fusion/utils/FusionStatus.java
@@ -1,8 +1,17 @@
 package org.xcolab.client.fusion.utils;
 
 public enum FusionStatus {
-    ACCEPTED,
-    REJECTED,
-    PENDING;
+    ACCEPTED("accepted"),
+    REJECTED("rejected"),
+    PENDING("pending");
 
+    private final String value;
+
+    FusionStatus(String value){
+        this.value=value;
+    }
+
+    public String getValue(){
+        return value;
+    }
 }
