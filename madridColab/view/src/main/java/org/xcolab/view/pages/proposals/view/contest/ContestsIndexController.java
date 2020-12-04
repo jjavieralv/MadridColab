20c7376b8da3fe31c7664b9c342c3439615f60cd
Juan Sebastian Ochoa ZAmbrano <juansebastian.ochoa@satec.es>
2020-12-04 18:12:51 +0100

    Update ContestsIndexController.java
    Added endpoint /contest/intercommunity

--- madridColab/view/src/main/java/org/xcolab/view/pages/proposals/view/contest/ContestsIndexController.java
+++ madridColab/view/src/main/java/org/xcolab/view/pages/proposals/view/contest/ContestsIndexController.java
@@ -74,6 +74,7 @@ public class ContestsIndexController extends BaseProposalsController {
         } else {
             final ContestType defaultContestType = new ContestType(ConfigurationAttributeKey.DEFAULT_CONTEST_TYPE_ID.get());
             final String originalUri = RequestUtil.getOriginalUri(request);
+
             if (!originalUri.startsWith(defaultContestType.getContestBaseUrl())) {
                 contestType =
                         ContestTypeClient.getActiveContestTypes().stream()
@@ -85,7 +86,7 @@ public class ContestsIndexController extends BaseProposalsController {
                 contestType = defaultContestType;
             }
         }
-
+        System.out.println(contestType);
         if (contestType.isRestrictedAccess() && !new ContestPermissions(loggedInMember)
                 .getCanAccessContests(contestType)) {
             return new AccessDeniedPage(loggedInMember).toViewName(response);
@@ -285,6 +286,237 @@ public class ContestsIndexController extends BaseProposalsController {
         setActivePageLink(model, contestType);
         return "/proposals/contestsIndex";
     }
+
+
+    @GetMapping("/contests/intercommunity")
+    public String showContestsIndexIntercommunity(HttpServletRequest request, HttpServletResponse response,
+            Model model, Member currentMember, ProposalContext proposalContext,
+            @RequestParam(required = false) String preferenceId,
+            @RequestParam(required = false) String viewType,
+            @RequestParam(required = false, defaultValue="false") boolean showAllContests,
+            @RequestParam(required = false, defaultValue = "" + FEATURED_COLLECTION_CARD_ID) long currentCollectionCardId,
+            @RequestParam(required = false) Long contestTypeId,
+            Member loggedInMember,
+            SortFilterPage sortFilterPage) {
+
+        ContestType contestType;
+        if (contestTypeId != null) {
+            contestType = new ContestType(contestTypeId);
+        } else {
+            final ContestType defaultContestType = new ContestType(ConfigurationAttributeKey.DEFAULT_CONTEST_TYPE_ID.get());
+            final String originalUri = "/contests";
+            if (!originalUri.startsWith(defaultContestType.getContestBaseUrl())) {
+                contestType =
+                        ContestTypeClient.getActiveContestTypes().stream()
+                                .filter(item -> originalUri
+                                        .startsWith(item.getContestBaseUrl()))
+                                //TODO: better exception --> 404
+                                .findFirst().orElseThrow(IllegalStateException::new);
+            } else {
+                contestType = defaultContestType;
+            }
+        }
+        System.out.println(contestType);
+        if (contestType.isRestrictedAccess() && !new ContestPermissions(loggedInMember)
+                .getCanAccessContests(contestType)) {
+            return new AccessDeniedPage(loggedInMember).toViewName(response);
+        }
+
+        final int totalContestCount = ContestClientUtil
+                .countContests(null, false, contestType.getId());
+
+        if (contestType.isSuggestionsActive()) {
+            Contest c = ContestClientUtil.getContest(contestType.getSuggestionContestId());
+            String link = c.getContestLinkUrl();
+            model.addAttribute("suggestionContestLink", link);
+        }
+
+        if (viewType == null) {
+            // view type wasn't set
+            final Cookie[] cookies = request.getCookies(); //null if cookies are disabled
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals(COOKIE_VIEW_TYPE)) {
+                        viewType = cookie.getValue();
+                    }
+                }
+            }
+        }
+        else {
+            // we need to change the view type
+            if (viewType.equals(VIEW_TYPE_GRID) || viewType.equals(VIEW_TYPE_LIST) || viewType.equals(VIEW_TYPE_OUTLINE)) {
+                // we should set the cookie but it doesn't work currently https://issues.liferay.com/browse/LPS-25733
+                // it should be handled in the view
+                response.addCookie(new Cookie(COOKIE_VIEW_TYPE, viewType));
+            }
+        }
+        if (viewType == null) {
+            viewType = VIEW_TYPE_DEFAULT;
+        }
+
+        /*--------------------------------*/
+        //Only for CollectionCards
+        /*--------------------------------*/
+
+        if (ConfigurationAttributeKey.COLAB_USES_CARDS.get() && !viewType.equals(VIEW_TYPE_OUTLINE)) {
+            Long ontologyTermToLoad;
+            boolean showCollectionCards=true;
+            boolean showOnlyFeatured = false;
+            if(sortFilterPage != null && sortFilterPage.getFilter() != null && !sortFilterPage.getFilter().isEmpty()) { //if search function was used
+                ontologyTermToLoad = null;
+                currentCollectionCardId = BY_TOPIC_COLLECTION_CARD_ID;
+                showCollectionCards=false;
+            } else if(currentCollectionCardId == FEATURED_COLLECTION_CARD_ID) {
+                showOnlyFeatured = true;  // filter with JSP  -> TODO COLAB-2627: increase performance
+                ontologyTermToLoad = null; //get all
+            } else {
+                ontologyTermToLoad = ContestClientUtil.getContestCollectionCard(currentCollectionCardId)
+                        .getOntology_term_to_load();
+            }
+
+            List<CollectionCardWrapper> collectionCards = new ArrayList<>();
+            LinkedList<CollectionCardWrapper> collectionHierarchy = new LinkedList<>();
+            if(showCollectionCards) { //don't display collectioncards if search results shown
+                for (ContestCollectionCard card: ContestClientUtil.getSubContestCollectionCards(currentCollectionCardId)) {
+                    collectionCards.add(new CollectionCardWrapper(card, viewType));
+                }
+                collectionCards.sort((o1, o2) ->
+                        o1.getOrder() < o2.getOrder() ? -1
+                                : o1.getOrder() == o2.getOrder() ? 0 : 1);
+                long tempId = currentCollectionCardId;
+                while(ContestClientUtil.getContestCollectionCard(tempId).getParent() != null) {
+                    collectionHierarchy.addFirst(new CollectionCardWrapper(ContestClientUtil.getContestCollectionCard(tempId), viewType));
+                    tempId = ContestClientUtil.getContestCollectionCard(tempId).getParent();
+                }
+                collectionHierarchy.addFirst(new CollectionCardWrapper(ContestClientUtil.getContestCollectionCard(tempId), viewType));
+                if(collectionHierarchy.size() == 1) {
+                    collectionHierarchy.clear();
+                }
+            }
+
+            model.addAttribute("collectionCards", new CollectionCardFilterBean(collectionCards));
+            model.addAttribute("currentCollectionCardId", currentCollectionCardId);
+            model.addAttribute("collectionHierarchy", collectionHierarchy);
+
+
+            model.addAttribute("showOnlyFeatured", showOnlyFeatured);
+
+            //if only featured
+            if(ContestClientUtil.getContestCollectionCard(currentCollectionCardId).getOntology_term_to_load() != null) {
+                model.addAttribute("ontologySpaceId", OntologyClientUtil.getOntologyTerm(ContestClientUtil.getContestCollectionCard(currentCollectionCardId).getOntology_term_to_load()).getOntologySpaceId());
+            } else {
+                model.addAttribute("ontologySpaceId", 0);
+            }
+        }
+
+        /*--------------------------------*/
+        //For other views
+        /*--------------------------------*/
+
+        List<Contest> contests = new ArrayList<>();
+
+        if (viewType.equals(VIEW_TYPE_OUTLINE)) {
+                                            
+            List<Contest> allContests = ContestClientUtil.getContests(
+                    null, false, contestType.getId()).stream()
+                    .filter(Contest::getShowInOutlineView)
+                    .collect(Collectors.toList());
+
+            final List<Contest> activeContests = allContests.stream()
+                    .filter(Contest::isContestActive)
+                    .collect(Collectors.toList());
+
+            List<Contest> otherContests;
+            if (showAllContests) {
+                contests = allContests;
+                otherContests = activeContests;
+            } else {
+                contests = activeContests;
+                otherContests = allContests;
+            }
+
+            List<OntologySpace> ontologySpacesRaw = OntologyClientUtil.getAllOntologySpaces();
+            List<OntologyTerm> ontologyTermsRaw = OntologyClientUtil.getAllOntologyTerms();
+            List<FocusArea> focusAreasRaw = OntologyClientUtil.getAllFocusAreas();
+            List<FocusAreaOntologyTerm> focusAreasOntologyTermsRaw = OntologyClientUtil.getAllFocusAreaOntologyTerms();
+            Map<Long, FocusArea> focusAreas = new TreeMap<>();
+
+            for (FocusArea area: focusAreasRaw) {
+                focusAreas.put(area.getId(), new FocusArea(area));
+            }
+
+            Map<Long, OntologySpace> ontologySpaces = new HashMap<>();
+            for (OntologySpace space: ontologySpacesRaw) {
+                ontologySpaces.put(space.getId(), new OntologySpace(space));
+            }
+
+            Map<Long, OntologyTerm> ontologyTerms = new TreeMap<>();
+            for (OntologyTerm term: ontologyTermsRaw) {
+                OntologyTerm termWrapped = new OntologyTerm(term);
+                final long ontologySpaceId = term.getOntologySpaceId();
+                final OntologySpace ontologySpace = ontologySpaces.get(ontologySpaceId);
+                if (ontologySpace == null) {
+                    throw new IllegalStateException(String.format(
+                            "Ontology space %d referenced by ontology term %d doesn't exist.",
+                            ontologySpaceId, term.getId()));
+                }
+                ontologySpace.addTerm(termWrapped);
+                ontologyTerms.put(term.getId(), termWrapped);
+            }
+
+            for (OntologyTerm term: ontologyTermsRaw) {
+                if (term.getParentId() > 0) {
+                    ontologyTerms.get(term.getId()).setParent(ontologyTerms.get(term.getParentId()));
+                }
+            }
+
+            for (FocusAreaOntologyTerm faTerm: focusAreasOntologyTermsRaw) {
+                focusAreas.get(faTerm.getFocusAreaId()).addOntologyTerm(ontologyTerms.get(faTerm.getOntologyTermId()));
+            }
+
+            List<OntologySpace> sortedSpaces = new ArrayList<>(ontologySpaces.values());
+            sortedSpaces.sort(Comparator.comparingInt(OntologySpace::getOrder));
+            model.addAttribute("focusAreas", focusAreas.values());
+            model.addAttribute("ontologyTerms", ontologyTerms.values());
+            model.addAttribute("ontologySpaces", sortedSpaces);
+            model.addAttribute("otherContests", otherContests);
+            model.addAttribute("contestType", contestType);
+        } else if (!ConfigurationAttributeKey.COLAB_USES_CARDS.get()) {
+            contests = ContestClientUtil.getContests(
+                    showAllContests ? null : true, false, contestType.getId());
+        }
+
+        model.addAttribute("showCollectionCards", ConfigurationAttributeKey.COLAB_USES_CARDS.get());
+        boolean showContestManagementLink = PermissionsClient.canAdminAll(currentMember) ;
+        model.addAttribute("showContestManagementLink", showContestManagementLink);
+        model.addAttribute("totalContestCount", totalContestCount);
+        model.addAttribute("contests", contests);
+        //not taken into account if collection cards enabled
+        model.addAttribute("showFilter", contests.size() >= MIN_SIZE_CONTEST_FILTER);
+        final ContestList sortedContests = new ContestList(contests, getComparator(sortFilterPage));
+        if (StringUtils.isEmpty(sortFilterPage.getFilter())) {
+            model.addAttribute("contestsSorted", sortedContests);
+        } else {
+            model.addAttribute("contestsSorted",
+                    sortedContests.getFiltered(getFilter(sortFilterPage)));
+        }
+        model.addAttribute("viewType", viewType);
+        model.addAttribute("sortFilterPage", sortFilterPage);
+        model.addAttribute("showAllContests", showAllContests);
+        model.addAttribute("showContestDisplayOptions", ConfigurationAttributeKey.SHOW_CONTESTS_DISPLAY_OPTIONS.get());
+
+
+        final String description = String.format("View %s %s run on the %s. ",
+                showAllContests ? "all" : "active",
+                contestType.getContestNamePluralLowercase(),
+                ConfigurationAttributeKey.COLAB_NAME.get())
+                + ConfigurationAttributeKey.META_PAGE_DESCRIPTION_CONTESTS.get();
+        model.addAttribute("pageDescription", description);
+
+        model.addAttribute("_activePageLink", "intercommunity");
+        return "/proposals/contestsIndex";
+    }
+
 
     private Comparator<Contest> getComparator(SortFilterPage sortFilterPage) {
         ContestsColumn sortColumn;
